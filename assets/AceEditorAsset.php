5b1fa7dedebe81dfb0b40849704a750bf47fd3be
Yuriy Basov <basowy@gmail.com>
2017-05-14 10:40:27 -0700

    ace-builds

---
+++ assets/AceEditorAsset.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author Yuriy Basov <basowy@gmail.com>
+ * @since 1.0.0
+ */
+
+
+namespace yii2x\common\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class AceEditorAsset extends AssetBundle
+{
+    public $publishOptions = [
+      'forceCopy'=>true,
+    ];    
+    public $sourcePath = '@vendor/yii2x/yii2-common/dist/font-awesome/';
+    public $css = [
+        'style.css'
+        ];
+    public $js =  [];
+    public $depends = [
+        'yii2x\common\assets\FontAwesomeBaseAsset'
+    ];
+}
